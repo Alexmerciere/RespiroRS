3d04dc82af74ce28ae558f214c2aa65266bccdec
Alexmerciere <alexandre.merciere@gmail.com>
2019-02-21 17:46:01 -1000

    update.21.02.19

--- R/row.plot.R
+++ R/row.plot.R
@@ -1,10 +1,11 @@
 
 ###Rowgraph O2 for all the chamber
 row.plot<-function(data,chamber.nber,unit,wayout){
-  x<-c(4,5,6,7,13,14,15,16,22,23,24,25,31,32,33,34)
+  x<-c("Ox.1","Ox.2","Ox.3","Ox.4","Ox.5","Ox.6","Ox.7","Ox.8","Ox.9","Ox.10","Ox.11","Ox.12","Ox.13","Ox.14","Ox.15","Ox.16")
+
  colchamber<-x[1:chamber.nber]
   for(i in colchamber){
-    c<-ggplot(data,environment = environment())+geom_point(aes(x=Timeabsolu2,y=as.numeric(as.character(data[,i]))))+ylab(paste(colnames(data[i]),unit,sep="")) +xlab("Time (sec)")
+    c<-ggplot(data,environment = environment())+geom_point(aes(x=Time.s,y=as.numeric(as.character(data[,i]))))+ylab(paste(colnames(data[i]),unit,sep="")) +xlab("Time (sec)")
     ggsave(c,filename=paste(colnames(data[i]),".pdf",sep=""),path =wayout,width=20, height=4)
     }
   }
