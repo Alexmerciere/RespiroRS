acd639226c151b1519e2444a1ed6b7b17900616a
Alexmerciere <alexandre.merciere@gmail.com>
2021-11-02 17:46:50 -1000

    UPDATE SMR function

--- R/SMR.R
+++ R/SMR.R
@@ -27,12 +27,9 @@ colnames(Result)<- c("fishID","Date", "Chamber","SMR","sdSMR","MaxMR","MinMR","F
 
   res$selection <- ifelse(res[, 5] %in% resstep3[, 5] & res[, 10] %in% resstep3[, 10], "SMR", ifelse(res[, 5] %in% resstep2[, 5] & res[, 10] %in% resstep2[, 10],"meanLowestO2valueOutlier", ifelse(res[, 5] %in% resstep1[, 5] & res[, 10] %in% resstep1[, 10], "NotUsedInSMRcalculation", "RsquaredOutlier")))
   #write Table
-  write.table(res, paste(wayout, "/", "resultchamber", Fishbase[ which(Fishbase$fishID==l),"Nb_Ch"], ".csv", sep = ""), sep = ";", dec = ".", row.names = F, qmethod = "double")
-  write.table(resblc, paste(wayout, "/", "resultblankchamber", Fishbase[ which(Fishbase$fishID==l),"Nb_Ch"],".csv", sep = ""), sep = ";", dec = ".", row.names = F, qmethod = "double")
-  write.table(resblcblc, paste(wayout, "/", "resultblankchamberblank.csv", sep = ""), sep = ";", dec = ".", row.names = F, qmethod = "double")
+  write.table(res, paste(wayout, "/", "resultchamber", Fishbase[ which(Fishbase$fishID==fishID),"Nb_Ch"], ".csv", sep = ""), sep = ";", dec = ".", row.names = F, qmethod = "double")
 
-
-  c<-ggplot(res,environment = environment())+geom_point(aes(x=res[,1]/3600,y=res[,5]),alpha=0.1)+ylab(colnames(res[5])) +xlab("Time (h)") +geom_point(aes(x=res[,1]/3600,y=MO2cor,colour=selection)) +scale_color_manual(values = c("NotUsedInSMRcalculation" = "black","RsquaredOutlier" = "red","meanLowestO2valueOutlier"="blue","SMR" = "green"))
+  c<-ggplot(res,environment = environment()) +xlab("Time (h)") +geom_point(aes(x=res[,1]/3600,y=res[,5],colour=selection)) +scale_color_manual(values = c("NotUsedInSMRcalculation" = "black","RsquaredOutlier" = "red","meanLowestO2valueOutlier"="blue","SMR" = "green"))
   ggsave(c,filename=paste("Chamber",Fishbase[ which(Fishbase$fishID==fishID),"Nb_Ch"],".pdf",sep=""),path = wayout,width=20, height=4)
   print(c)
   #####################################Final Table###################################################
@@ -40,10 +37,10 @@ colnames(Result)<- c("fishID","Date", "Chamber","SMR","sdSMR","MaxMR","MinMR","F
   Result[which(fishID==fishID),1]<-fishID
   Result[which(fishID==fishID),2]<-res$Date[[1]]
   Result[which(fishID==fishID),3]<-Fishbase[ which(Fishbase$fishID==fishID),"Nb_Ch"]
-  Result[which(fishID==fishID),4]<-mean(subset(res,res$selection=="SMR")$MO2cor)
-  Result[which(fishID==fishID),5]<-sd(subset(res,res$selection=="SMR")$MO2cor)
-  Result[which(fishID==fishID),6]<-max(subset(res,!res$selection=="RsquaredOutlier")$MO2cor)
-  Result[which(fishID==fishID),7]<-min(subset(res,res$selection=="SMR")$MO2cor)
+  Result[which(fishID==fishID),4]<-mean(subset(res,res$selection=="SMR")[,5])
+  Result[which(fishID==fishID),5]<-sd(subset(res,res$selection=="SMR")[,5])
+  Result[which(fishID==fishID),6]<-max(subset(res,!res$selection=="RsquaredOutlier")[,5])
+  Result[which(fishID==fishID),7]<-min(subset(res,res$selection=="SMR")[,5])
 
 
   ###FirstMR###
